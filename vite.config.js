2a283f1a43229767e5477edaa83091f8cd8acc65
Raphael Pacheco <raphaelrogen@gmail.com>
2023-07-31 20:27:04 -0300

    configs do deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RaphaelPach.github.io/',
+  base: '/RaphaelPach.github.io',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
